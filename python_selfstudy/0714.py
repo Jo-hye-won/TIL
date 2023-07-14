b6b107791ab050a5c7f4543b87ac0f739e392cea
조혜원 <hws9701@naver.com>
2023-07-14 17:50:51 +0900

    7/14 최종

---
+++ python_selfstudy/0714.py
@@ -0,0 +1,70 @@
+
+# for num in range(3):
+#     print(num)
+
+# for y in range(1,10):
+#     print(2,'x',y,'=',2*y)
+
+
+# print('직각\n삼각형\n그리기')
+
+# leg = int(input('변의길이:'))
+# for i in range(leg):
+#     print('#'*(i+1))
+
+# num = int(input())
+# i=0
+# while i<num :
+   
+#     print('',num)
+#     i+=1
+
+
+# a=list(map(int,input().split()))
+# # print(a)
+
+# input_number = int(input())
+# if input_number <0:
+#     print(input_number*-1)
+# elif input_number >=0:
+#     print(input_number)
+
+
+# h=100
+# w=3/5
+
+# i=1
+# while i<= 10:
+#     h=h*w
+#     print(i,round(h,2))
+#     i=i+1
+
+
+# max=10
+# while True:
+#     num=int(input())
+#     if num>max:
+#         print(num,"is too big")
+#         break
+    
+# # 단위기호
+# num=int(input())
+# result = str(num)
+
+# if num >=1000:
+#     result =str(num//1000)+"K"
+# elif num >=0:
+#     pass
+
+# print(result)
+
+
+# #양수만 덧셈하기
+# sum=0
+# while True:
+#     num = int(input())
+#     if num<0:
+#         break
+#     else:
+#         sum+=num # sum=sum+num 이라는 뜻
+# print(sum)
